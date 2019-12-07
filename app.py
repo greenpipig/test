c0278056863c00a28e918ee6ce93fef8036447d4
greenpipig <1105263198@qq.com>
2019-12-07 18:26:16 +0800

    test

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask,url_for,request,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form['user'] == 'admin':
+            return 'Admin login successfully!'
+        else:
+            return 'No such user!'
+    title = request.args.get('title', 'Default')
+    return render_template('login.html', title=title)
+
+if __name__ == '__main__':
+    app.run(host=" 10.28.242.8",port=80,debug=True)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
